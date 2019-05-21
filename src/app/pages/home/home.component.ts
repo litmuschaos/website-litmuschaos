4a9bd7f6b8ceab11e1714364425fb6fd9a39d7db
inyee786 <intakhab.ali@mayadata.io>
2019-05-21 14:14:11 +0530

    add litmus command

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,14 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  public copyText:string = "Copy";
+  public copyText1:string = "Copy";
+  public copyText2:string = "Copy";
+  public copyText3:string = "Copy";
   constructor() { }
 
   ngOnInit() {
 
   }
-  public CopyButton(){
-    this.copyText="Copied"
+  public CopyButton1(){
+    this.copyText1="Copied"
+  }
+  public CopyButton2(){
+    this.copyText2="Copied"
+  }
+  public CopyButton3(){
+    this.copyText3="Copied"
   }
 
 }
