490e20a6873cab4a9c4fc71571ce812cff15f4c0
inyee786 <intakhab.ali@mayadata.io>
2019-09-16 21:49:23 +0530

    add github button

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-header',
@@ -8,15 +10,17 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   public hide = false;
-
+  public starCount:number = 0;
   notification = {
     notification_content: 'Get OpenEBS Commercial Support',
     notification_url: 'https://mayadata.io/mdap'
   };
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get("http://localhost:8080/getstars").subscribe(res => this.starCount=res["star-count"]);
+    console.log(this.starCount);
   }
 
 }
