8569d913d5643803a3a31429560fc27a3caa6c35
inyee786 <intakhab.ali@mayadata.io>
2019-09-17 08:39:37 +0530

    add rest api end point for github

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -11,15 +11,25 @@ export class HeaderComponent implements OnInit {
 
   public hide = false;
   public starCount:number = 0;
+  private host:any;
+  private githubApiurl;
   notification = {
     notification_content: 'Get OpenEBS Commercial Support',
     notification_url: 'https://mayadata.io/mdap'
   };
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.host = window.location.host;
+    if ((this.host.toString().indexOf("localhost") + 1) && this.host.toString().indexOf(":")) {
+      this.githubApiurl = "http://localhost:3000/";
+
+    } else {
+      this.githubApiurl = "/github";
+    }
+   }
 
   ngOnInit() {
-    this.http.get("http://localhost:8080/getstars").subscribe(res => this.starCount=res["star-count"]);
+    this.http.get(this.githubApiurl+'/getstars').subscribe(res => this.starCount=res["star-count"]);
     console.log(this.starCount);
   }
 
