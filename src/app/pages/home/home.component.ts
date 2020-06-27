1f2929f127ee9e9eaa404a96882f78ecad739a3c
Arkajyoti Mukherjee <32966391+arkajyotiMukherjee@users.noreply.github.com>
2020-06-27 15:05:45 +0530

    Changed the footer, added cncf sandbox branding, added new cover image (#70)
    * Changed the footer, added cncf sandbox branding, added a new cover image
    Signed-off-by: arkajyotiMukherjee <arkajyoti31@gmail.com>

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -17,28 +17,34 @@ export class HomeComponent implements OnInit {
 
   communities = [
     {
-      src: 'assets/images/footer/sl.svg',
+      src: 'assets/images/community/slack-new-logo.svg',
       alt: 'slack',
       url: 'https://app.slack.com/client/T09NY5SBT/CNXNB0ZTN',
       name: 'Join #litmus Channel'
     },
     {
-      src: 'assets/images/footer/gh.svg',
+      src: 'assets/images/community/github-1.svg',
       alt: 'github',
       url: 'https://github.com/litmuschaos',
-      name: 'Github'
+      name: 'GitHub'
     },
     {
-      src: 'assets/images/footer/tw.svg',
+      src: 'assets/images/community/twitter-3.svg',
       alt: 'twitter',
       url: 'https://twitter.com/LitmusChaos',
       name: 'Twitter'
     },
     {
-      src: 'assets/images/footer/md.svg',
-      alt: 'Blog',
+      src: 'assets/images/community/devto.svg',
+      alt: 'DEV',
       url: 'https://dev.to/t/litmuschaos/latest',
-      name: 'Blog'
+      name: 'DEV'
+    },
+    {
+      src: 'assets/images/community/youtube-3.svg',
+      alt: 'Youtube',
+      url: 'https://www.youtube.com/channel/UCa57PMqmz_j0wnteRa9nCaw ',
+      name: 'Youtube'
     },
   ];
 
