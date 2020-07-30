f697ebbd6206d030f5a05bc05f4c7307c5a22fb8
Sayan Mondal <sayan.mondal@mayadata.io>
2020-07-30 16:10:37 +0530

    Added Cloud Native way Div
    Signed-off-by: Sayan Mondal <sayan.mondal@mayadata.io>

--- src/components/button/index.ts
+++ src/components/button/index.ts
@@ -1,6 +1,5 @@
 export {
   Button,
-  ButtonMobile,
   OutlinedButton,
   WhiteOnGreenButton
 } from "./button";
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,44 @@
 import React from "react";
 import { Head } from "../components/containers/index";
-import { Layout } from "../components/layout";
+import { Layout, ResponsiveRow } from "../components/layout";
 import { SEO } from "../components/seo";
+import {useTheme} from '../styles'
 
-const App = () => (
+const CloudNativeWay:React.FC = ({ children }) => {
+  const { sm } = useTheme().screens;
+  if(sm) {
+    return (
+      <div style={{margin: '0 1rem'}}>
+        {children}
+      </div>
+    );
+  } else {
+    return (
+      <div style={{margin: '9rem 0 3rem 0', textAlign: 'center'}}>
+          {children}
+      </div>
+    );
+  }
+};
+
+const App = () => {
+
+  return (
   <Layout>
     <SEO />
     <Head />
+    <CloudNativeWay>
+      <h2>Do it the cloud-native way</h2>
+      <ResponsiveRow>
+        <img src="./svg/chaosbird_experimenting.svg" alt="Chaos Bird Experimenting" />
+        <div>
+          <p>Some</p>
+          <p>File</p>
+        </div>
+      </ResponsiveRow>
+    </CloudNativeWay>
   </Layout>
-);
+  );
+};
 
 export default App;
