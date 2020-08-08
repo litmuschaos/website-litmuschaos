a403cfd6554fdba6a2aafd5c6db7486088648d14
MAYUKH MALLICK <43519140+mallick370@users.noreply.github.com>
2020-08-08 22:16:09 +0530

    Updated Adopter Section
    *Made the adopter responsive for all the media screens (mobile/tablets/desktops)*
    
    Signed-off-by: MAYUKH MALLICK <mallick370@gmail.com>

--- src/components/adopter/adopter.tsx
+++ src/components/adopter/adopter.tsx
@@ -22,11 +22,23 @@ const Mayadata: React.FC = () => {
   
 };
 const Wipro: React.FC = () => {
-  const { sm } = useTheme().screens
+  const { sm,md,lg } = useTheme().screens
   if(sm )
   {
     return(
       <img className="WiproLogo" width="50vw" src="/svg/Wipro.svg" alt="Mayadata Logo" />
+    )
+  }
+  else if(md)
+  {
+    return(
+      <img className="WiproLogo" style={{width: "3rem"}} src="/svg/Wipro.svg" alt="Mayadata Logo" />
+    )
+  }
+  else if(lg )
+  {
+    return(
+      <img className="WiproLogo" style={{width: "4rem"}} src="/svg/Wipro.svg" alt="Mayadata Logo" />
     )
   }
   else
@@ -38,11 +50,24 @@ const Wipro: React.FC = () => {
 };
 
 const OpenEBS: React.FC = () => {
-  const { sm } = useTheme().screens
+  const { sm,lg,md } = useTheme().screens
   if(sm )
   {
     return(
       <img className="OpenEBSLogo" width="50vw"src="/svg/OpenEBS.svg" alt="Mayadata Logo" />
+    )
+  }
+  else if (md)
+  {
+    return(
+      <img className="OpenEBSLogo"  style={{width: "3rem"}} src="/svg/OpenEBS.svg" alt="Mayadata Logo" />
+    )
+  }
+  
+  else if(lg )
+  {
+    return(
+      <img className="OpenEBSLogo"  style={{width: "4rem"}} src="/svg/OpenEBS.svg" alt="Mayadata Logo" />
     )
   }
   else
@@ -54,11 +79,23 @@ const OpenEBS: React.FC = () => {
 }
 ;
 const Intuit: React.FC = () => {
-  const { sm } = useTheme().screens
+  const { sm,lg,md } = useTheme().screens
   if(sm )
   {
     return(
       <img  className="IntuitLogo" width="50vw" src="/svg/Intuit.svg" alt="Mayadata Logo" />
+    )
+  }
+  else if(md)
+  {
+    return(
+      <img  className="IntuitLogo" style={{width: "3rem"}} src="/svg/Intuit.svg" alt="Mayadata Logo" />
+    )
+  }
+  else if(lg)
+  {
+    return(
+      <img  className="IntuitLogo" style={{width: "4rem"}} src="/svg/Intuit.svg" alt="Mayadata Logo" />
     )
   }
   else{
@@ -70,11 +107,23 @@ const Intuit: React.FC = () => {
 };
 
 const Zerebrium: React.FC = () => {
-  const { sm } = useTheme().screens
+  const { sm,md,lg } = useTheme().screens
   if(sm)
   {
     return(
       <img   className="ZerebriumLogo" width="50vw" src="/svg/Zerebrium.svg" alt="Mayadata Logo" />
+    )
+  }
+  else if (md)
+  {
+    return(
+      <img   className="ZerebriumLogo" style={{width: "3rem"}} src="/svg/Zerebrium.svg" alt="Mayadata Logo" />
+    )
+  }
+  else if(lg)
+  {
+    return(
+      <img   className="ZerebriumLogo" style={{width: "4rem"}} src="/svg/Zerebrium.svg" alt="Mayadata Logo" />
     )
   }
   else
@@ -88,13 +137,21 @@ const Zerebrium: React.FC = () => {
 };
 
 const CreatedByColumn: React.FC = () => {
-  const { sm } = useTheme().screens
+  const { sm,lg } = useTheme().screens
   if (sm) {
     return <div style={{ margin: '0 1rem' }}>
       <h5 className="AdopterHeading">Created By</h5>
             <Mayadata />
     </div>
-  } else {
+  } 
+  else if(lg)
+  {
+    return <div style={{ margin: '1rem' }}>
+      <h5 className="AdopterHeading">Created By</h5>
+            <Mayadata />
+    </div>
+  }
+  else {
     return (
       <div style={{ marginBottom:'3rem' }}>
         <h5 className="AdopterHeading">Created By</h5>
@@ -107,7 +164,7 @@ const CreatedByColumn: React.FC = () => {
 
 
 const AdopterColumn: React.FC = () => {
-  const { sm } = useTheme().screens
+  const { sm,lg } = useTheme().screens
   if (sm) {
     return <div style={{ margin: '0 auto 0 1rem' , width :"80vw" } }>
       <h5 className="AdopterHeading">Adopted By</h5>
@@ -116,7 +173,18 @@ const AdopterColumn: React.FC = () => {
               <Wipro /><OpenEBS /><Intuit /><Zerebrium />
             </div>  
     </div>
-  } else {
+  } 
+  else if(lg)
+  {
+    return <div style={{ margin: '1rem' , width :"24rem" } }>
+    <h5 className="AdopterHeading">Adopted By</h5>
+          <div className="AdopterLogos">
+             
+            <Wipro /><OpenEBS /><Intuit /><Zerebrium />
+          </div>  
+  </div>
+  }
+  else {
     return (
       <div style={{ margin:'0 0 3rem 7rem' ,width : "27.5rem"}}>
         <h5 className="AdopterHeading">Adopted By</h5>
@@ -131,7 +199,7 @@ const AdopterColumn: React.FC = () => {
 
 const JoinAsAdopter: React.FC = () => {
     
-    const { sm } = useTheme().screens
+    const { sm,lg} = useTheme().screens
     const flexibleButton = (
       <Button screen={sm ? "small" : "large"} style={{ marginTop :"-4rem"}} gradientColor="green">
         List Yourself As an Adopter
@@ -143,7 +211,19 @@ const JoinAsAdopter: React.FC = () => {
           <span style={{color : "#6F6F6F", fontSize: "0.875rem", fontWeight : "normal"}}>and share your chaos engineering story to the community.</span></h5>
           {flexibleButton}
         </div>
-    } else {
+    } 
+    else if(lg)
+    {
+      return (
+        <div style={{ margin:'1rem', maxWidth : "25rem"}}>
+          <h5 className="AdopterHeading">Add yourself to the adopters <br/>
+          <span style={{color : "#6F6F6F", fontSize: "1rem", fontWeight : "normal"}}>and share your chaos engineering story to the community.</span></h5>
+          <button style={{width : "100%", height: "3.75rem", borderRadius: "0.1875rem", border:"none",color: "white", backgroundImage : "linear-gradient(135.47deg, #109C68 1.77%, #25C087)"}} >
+            List yourself as adopter</button>
+        </div>
+      )
+    }
+    else {
       return (
         <div style={{ margin:'0 0rem 3rem 7rem', maxWidth : "20.625rem"}}>
           <h5 className="AdopterHeading">Add yourself to the adopters <br/>
@@ -156,18 +236,23 @@ const JoinAsAdopter: React.FC = () => {
 
 
 const Adopter: React.FC = () => {
-    
+  const { lg } = useTheme().screens;
   
     
-    return (
+  return (
     <>
-      <ResponsiveRow>
-          <CreatedByColumn />
-          <AdopterColumn />
-          <JoinAsAdopter />
-      </ResponsiveRow>
-    </>
-    );  
+                     
+            <ResponsiveRow>
+                <CreatedByColumn />
+                <AdopterColumn />
+                <JoinAsAdopter />
+                  
+              
+            </ResponsiveRow>
+          
+        
+  </>
+  );  
 };
 
 export { Adopter };
