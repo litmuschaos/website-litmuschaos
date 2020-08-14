62267a7502903320f5b8b5f4fffd3d37e08765b0
Sayan Mondal <sayan.mondal@mayadata.io>
2020-08-14 21:44:01 +0530

    404 page updated
    Signed-off-by: Sayan Mondal <sayan.mondal@mayadata.io>

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,39 +1,50 @@
 import { Link } from "gatsby";
+import styled from "styled-components";
 import React from "react";
 import { Image404 } from "../components/imageFetch/Image404";
+
+const Background = styled.img`
+  position: absolute;
+  height: 100%;
+  z-index: -1;
+`;
 
 // Component
 const NotFoundPage: React.FC = () => {
   return (
-    <div style={{ display: "flex" }}>
-      <div style={{ width: "50%", margin: "8% 0" }}>
-        <h1 style={{ width: "100%", margin: "3% 11rem", fontWeight: "bold" }}>
-          Whoops!
-          <br />
-          This page is unavailable
-        </h1>
-        <p style={{ width: "50%", margin: "0 11rem" }}>
-          The page does not exist, or please try again later
-        </p>
+    <>
+      <Background src="/svg/404Background.svg" alt="404 Background" />
+      <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+        <div style={{ margin: "10% 0" }}>
+          <h1 style={{ width: "100%", margin: "3% 40%", fontWeight: "bold" }}>
+            Whoops!
+            <br />
+            This page is unavailable
+          </h1>
+          <p style={{ width: "100%", margin: "0 40%" }}>
+            The page does not exist, or please try again later
+          </p>
 
-        <Link to="/">
-          <button
-            style={{
-              margin: "5rem 11rem",
-              padding: "1rem 2rem",
-              background: "#5B44BA",
-              borderRadius: "0.3rem",
-              color: "#ffff",
-            }}
-          >
-            Go back home
-          </button>
-        </Link>
+          <Link to="/">
+            <button
+              style={{
+                margin: "5rem 40%",
+                padding: "1rem 2rem",
+                width: "50%",
+                background: "#5B44BA",
+                borderRadius: "0.3rem",
+                color: "#ffff",
+              }}
+            >
+              Go back home
+            </button>
+          </Link>
+        </div>
+        <div style={{ width: "50%", margin: "8% 0", height: "100%" }}>
+          <Image404 />
+        </div>
       </div>
-      <div style={{ width: "50%", margin: "8% 0" }}>
-        <Image404 />
-      </div>
-    </div>
+    </>
   );
 };
 
