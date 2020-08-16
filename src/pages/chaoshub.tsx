709c9a81df3896fe7df524366a9157f68494bf9e
arkajyotiMukherjee <arkajyoti.mukherjee@mayadata.io>
2020-08-16 16:46:11 +0530

    404 page support for nginx
    Signed-off-by: arkajyotiMukherjee <arkajyoti.mukherjee@mayadata.io>

--- src/pages/chaoshub.tsx
+++ src/pages/chaoshub.tsx
@@ -11,12 +11,7 @@ import { SEO } from "../components/seo";
 const ChaosHub = () => {
   return (
     <Layout>
-      <SEO
-        title="ChaosHub"
-        description="Chaos Engineering redefined: LitmusChaos is an open source cloud-native Chaos Engineering framework which helps orchestrate chaos on Kubernetes platforms."
-        url="staging.litmuschaos.io/chaoshub"
-        author="MayaData"
-      />
+      <SEO />
       <Head />
       <WhyChaoshub />
       <ChaoshubStatistics />
