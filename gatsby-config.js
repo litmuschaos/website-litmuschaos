4c5c9783540a47ebd890f36e5f537a58f0187890
arkajyotiMukherjee <arkajyoti.mukherjee@mayadata.io>
2020-08-16 17:42:30 +0530

    added google analytics and google tag manager, fixed terminal text for run chaos
    Signed-off-by: arkajyotiMukherjee <arkajyoti.mukherjee@mayadata.io>

--- gatsby-config.js
+++ gatsby-config.js
@@ -103,5 +103,36 @@ module.exports = {
       },
     },
     `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-92076314-26",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // The IP anonymization feature in Google Analytics anonymizes the last digits of the user's IP (optional)
+        anonymize: true,
+        // Respect Do Not Track (optional)
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/404/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 500,
+        // Defers execution of google analytics script after page load
+        defer: true,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "litmuschaos.io",
+      },
+    },
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "UA-92076314-26",
+        includeInDevelopment: false,
+        defaultDataLayer: { platform: "gatsby" },
+      },
+    },
   ],
 };
