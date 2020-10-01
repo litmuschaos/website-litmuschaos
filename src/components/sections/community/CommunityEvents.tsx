d28a2d092e6968dad0fe419f89e33eecbf1ff880
Sayan Mondal <sayan.mondal@mayadata.io>
2020-10-01 12:46:20 +0530

    Added HacktoberFest Card (#124) (#125)
    * Added HacktoberFest Card
    * Updated Hacktober Logo to Latest 2020
    * Updated Hacktober link + Updated text
    
    Signed-off-by: Sayan Mondal <sayan.mondal@mayadata.io>

--- src/components/sections/community/CommunityEvents.tsx
+++ src/components/sections/community/CommunityEvents.tsx
@@ -7,7 +7,7 @@ import {
 } from "../../../components/layout";
 import { Heading, Paragraph } from "../../../components/texts";
 import { CNCFImage } from "../../image-fetch/CNCF";
-import { CommunitySyncUpImage } from "../../image-fetch/CommunitySyncUp";
+import { HacktoberFestImage } from "../../image-fetch/HacktoberFest";
 
 const CommunityEvents: React.FC = () => {
   return (
@@ -26,6 +26,24 @@ const CommunityEvents: React.FC = () => {
         </BoundedContainer>
         <BoundedContainer breakpoint="xl" width="40rem" margin="0 auto">
           <ResponsiveRow breakpoint="xl">
+            {/* <Card
+              width="100%"
+              height="100%"
+              subheading="Community Sync Up"
+              body="Meet and learn from the maintainers, contributors, and developers of Litmus everything about Chaos Engineering."
+              image={<CommunitySyncUpImage />}
+              link="https://zoom.us/j/91358162694"
+            /> */}
+
+            <Card
+              width="100%"
+              height="100%"
+              subheading="HacktoberFest"
+              body="Contribute to LitmusChaos and get a chance to win not only swags but also a grand prize and much more"
+              image={<HacktoberFestImage />}
+              link="https://mayadata.io/hacktoberfest-2020"
+            />
+
             <Card
               width="100%"
               height="100%"
@@ -33,15 +51,6 @@ const CommunityEvents: React.FC = () => {
               body=" Discussion about the best practices and tutorials for practicing chaos engineering"
               image={<CNCFImage />}
               link="https://community.cncf.io/events/details/cncf-kubernetes-chaos-engineering-meetup-group-presents-kubernetes-chaos-engineering-meetup-1/#/"
-            />
-
-            <Card
-              width="100%"
-              height="100%"
-              subheading="Community Sync Up"
-              body="Meet and learn from the maintainers, contributors, and developers of Litmus everything about Chaos Engineering."
-              image={<CommunitySyncUpImage />}
-              link="https://zoom.us/j/91358162694"
             />
           </ResponsiveRow>
         </BoundedContainer>
