76c8f6a590d85073dc5745a5204e3cb5208b86ec
Sayan Mondal <sayan.mondal@mayadata.io>
2020-10-01 11:06:39 +0530

    Added HacktoberFest Card (#124)
    * Added HacktoberFest Card
    * Updated Hacktober Logo to Latest 2020
    * Updated Hacktober link + Updated text
    
    Signed-off-by: Sayan Mondal <sayan.mondal@mayadata.io>

---
+++ src/components/image-fetch/HacktoberFest.tsx
@@ -0,0 +1,47 @@
+import { graphql, useStaticQuery } from "gatsby";
+import Img from "gatsby-image";
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const StyledImage = styled(Img)`
+  margin: auto;
+  width: 100%;
+  height: ${(props) =>
+    props.theme.screens.sm
+      ? "8rem"
+      : props.theme.screens.xl
+      ? "10rem"
+      : "10rem"};
+  object-fit: contain;
+`;
+
+const HacktoberFestImage: React.FC = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      image: file(relativePath: { eq: "hacktoberfest.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 1024, quality: 100) {
+            ...GatsbyImageSharpFluid_withWebp
+            ...GatsbyImageSharpFluidLimitPresentationSize
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <Wrapper>
+      <StyledImage
+        fluid={data.image.childImageSharp.fluid}
+        alt="Image of Litmus Features"
+      />
+    </Wrapper>
+  );
+};
+
+export { HacktoberFestImage };
