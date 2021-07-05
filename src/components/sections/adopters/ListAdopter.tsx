46ab99b6e63b6ae3f3bf99b7f62dcadc9bcdc930
Hrishav Kumar <hrishav@chaosnative.com>
2021-07-05 10:52:23 +0530

    Renamed files
    Signed-off-by: Hrishav Kumar <hrishav@chaosnative.com>

--- src/components/sections/adopters/ListAdopter.tsx
+++ src/components/sections/adopters/ListAdopter.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import styled, { useTheme } from "styled-components";
 import { getAdopterCardImg } from "../../image-fetch/AdopterCardImg";
 import { Center, ResponsiveRow, SectionDark } from "../../layout";
-import { AdopterList } from "./Adopters.data";
+import { adopterList } from "./adopters.data";
 interface IProps {
   bgColor?: string;
   flexDir?: boolean;
@@ -29,26 +29,26 @@ interface AdopterTheme {
   link?: string;
 }
 const CompanyLogoCard = styled.div<AdopterTheme>`
-  width: ${(props) => (props.theme.screens.lg ? "100%" : "500px")};
+  width: ${props => (props.theme.screens.lg ? "100%" : "500px")};
   border-radius: 10px;
   display: flex;
-  background: ${(props) => props.theme.colors[`CTA${props.link}`]};
+  background: ${props => props.theme.colors[`CTA${props.link}`]};
   align-items: center;
   justify-content: center;
   height: 300px;
-  margin-bottom: ${(props) => (props.theme.screens.lg ? "2rem" : "0")};
+  margin-bottom: ${props => (props.theme.screens.lg ? "2rem" : "0")};
   box-shadow: 0 30px 60px -10px rgba(0, 0, 0, 0.3);
 `;
 
 const CompanyInfoCard = styled.div<FlexDir>`
   display: flex;
-  height: ${(props) => (props.theme.screens.lg ? "auto" : "300px")};
+  height: ${props => (props.theme.screens.lg ? "auto" : "300px")};
   flex-direction: column;
-  justify-content: ${(props) =>
+  justify-content: ${props =>
     props.theme.screens.lg ? "flex-start" : "center"};
-  margin-top: ${(props) => (props.theme.screens.lg ? "1rem" : "0")};
-  ${(props) => (props.theme.screens.lg ? "" : "width: 60%;")}
-  ${(props) =>
+  margin-top: ${props => (props.theme.screens.lg ? "1rem" : "0")};
+  ${props => (props.theme.screens.lg ? "" : "width: 60%;")}
+  ${props =>
     props.theme.screens.lg
       ? ""
       : props.reverse
@@ -57,18 +57,18 @@ const CompanyInfoCard = styled.div<FlexDir>`
 `;
 
 const CompanyInfoCardHead = styled.h2`
-  font-size: ${(props) => (props.theme.screens.lg ? "18px" : "32px")};
-  line-height: ${(props) => (props.theme.screens.lg ? "30px" : "40px")};
+  font-size: ${props => (props.theme.screens.lg ? "18px" : "32px")};
+  line-height: ${props => (props.theme.screens.lg ? "30px" : "40px")};
 `;
 
 const CompanyInfoCardText = styled.p`
-  font-size: ${(props) => (props.theme.screens.lg ? "14px" : "16px")};
-  line-height: ${(props) => (props.theme.screens.lg ? "24px" : "26px")};
+  font-size: ${props => (props.theme.screens.lg ? "14px" : "16px")};
+  line-height: ${props => (props.theme.screens.lg ? "24px" : "26px")};
   margin: 0.5rem 0;
   color: #6f6f6f;
 `;
 
-const ListCard: React.FC<IProps> = (props) => {
+const ListCard: React.FC<IProps> = props => {
   const { textSecondary } = useTheme().colors;
   const images = getAdopterCardImg();
   return (
@@ -100,7 +100,7 @@ const ListCard: React.FC<IProps> = (props) => {
 const ListAdopters: React.FC<IProps> = ({ url }) => (
   <SectionDark>
     <MainAdopterCont>
-      {AdopterList.map((adopter) => {
+      {adopterList.map(adopter => {
         return (
           <ListCard
             reverse={adopter.reverse}
