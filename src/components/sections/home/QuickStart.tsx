b2a089f2dd0a6295bd16902695c7d980566b0a71
Hrishav <hrishav@chaosnative.com>
2021-08-03 16:23:11 +0530

    Updated community video (#172)
    * Updated community video
    
    Signed-off-by: Hrishav Kumar <hrishav@chaosnative.com>
    
    * Updated introduction video
    
    Signed-off-by: Hrishav Kumar <hrishav@chaosnative.com>

--- src/components/sections/home/QuickStart.tsx
+++ src/components/sections/home/QuickStart.tsx
@@ -14,19 +14,19 @@ const FadedDiv = styled.div`
   display: flex;
   justify-content: space-between;
   margin-top: 4.5rem;
-  margin-bottom: ${props => (props.theme.screens.xl ? "10rem" : "5rem")};
-  ${props =>
+  margin-bottom: ${(props) => (props.theme.screens.xl ? "10rem" : "5rem")};
+  ${(props) =>
     props.theme.screens.md
       ? css`
           width: 0.625rem;
           margin-bottom: 0;
           flex-direction: column;
-          background: ${props => props.theme.gradient.fadedLine(0)};
+          background: ${(props) => props.theme.gradient.fadedLine(0)};
         `
       : css`
           width: 100%;
           height: 0.625rem;
-          background: ${props => props.theme.gradient.fadedLine(90)};
+          background: ${(props) => props.theme.gradient.fadedLine(90)};
         `}
 `;
 
@@ -36,7 +36,7 @@ const KnobOuter = styled.div`
   background: white;
   border-radius: 50%;
   box-shadow: 0px 9px 11px rgba(91, 68, 186, 0.15);
-  ${props =>
+  ${(props) =>
     props.theme.screens.md
       ? css`
           position: absolute;
@@ -55,9 +55,9 @@ const KnobOuter = styled.div`
 const KnobInner = styled.div`
   display: grid;
   place-items: center;
-  background: ${props => props.theme.colors.darkGreen(1)};
+  background: ${(props) => props.theme.colors.darkGreen(1)};
   border-radius: 50%;
-  ${props =>
+  ${(props) =>
     props.theme.screens.md
       ? css`
           width: 0.625rem;
@@ -110,7 +110,7 @@ const QuickStart: React.FC = () => {
             </BoundedContainer>
             <VideoFrame
               width="40rem"
-              src="https://www.youtube.com/embed/rOrKegj5ePI"
+              src="https://www.youtube.com/embed/P3ky0CfLxnE"
             />
           </ResponsiveRow>
         </Center>
